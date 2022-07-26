c0b64a958e62bd47565c435c42a942116e46b8c6
Lilia Lomaka <llomaka80@gmail.com>
2022-07-26 14:31:03 +0300

    Імпорт fs та path

---
+++ contacts.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+//  * Розкоментуйте і запиши значення
+const contactsPath = path.resolve('contacts.js');
+console.log(contactsPath);
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
